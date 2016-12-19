633ab3fed2f7bb05285782a4771ecf2b0797e1b5
Emanuele Ballarin (ehteqx) <ehteqx@gmail.com>
2016-12-19 14:46:59 +0100

    Sort-of first commit

---
+++ boltzmann.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/julia
+
+# BoltzMAnn - A simulated-annealing-powered (Restricted) Boltzmann Machine
+# Copyright (c) 2016 Emanuele Ballarin <emanuele@ballarin.cc>
+# Software released under the terms of the MIT License
+
+# The purpose of the script is to implement a computationally-efficient and
+# straightforward (optionally Restricted) Boltzmann Machine, powered under the
+# hood by Simulated Annealing as a means to 'thermalize' the Hopfield net.
+# The network has been developed to be as general-purpose as possible and the
+# choice of the Julia language was determined as the optimal compromise between
+# runtime performance, conciseness, expressiveness and high-level clarity.
+
+##############################################################################
+# Placeholder
+##############################################################################
+
+println("Hello! Work in progress...")
